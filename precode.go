444dd5ee552758edd4dc6a89063c25f096f7c714
Анастасия Сребницкая <srea@automacon.ru>
2024-09-10 19:52:04 +0300

    1

--- precode.go
+++ precode.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -12,21 +14,48 @@ import (
 // вызывается функция fn. Она служит для подсчёта количества и суммы
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
-	// 1. Функция Generator
-	// ...
+	defer close(ch)
+	var n int64 = 1
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// Отправляем сгенерированное число в канал
+			ch <- n
+			// Вызываем переданную функцию для подсчёта
+			fn(n)
+			// Генерируем следующее число
+			n++
+			// Добавим небольшую паузу для наглядности
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
-	// 2. Функция Worker
-	// ...
+	for {
+		v, ok := <-in
+		if !ok {
+			// Если канал in закрыт, закрываем канал out и выходим
+			close(out)
+			return
+		}
+		// Отправляем значение в канал out
+		out <- v
+		// Делаем паузу в 1 миллисекунду
+		time.Sleep(1 * time.Millisecond)
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithCancel(context.Background())
+	// через 1 секунду вызываем cancel для отмены операции
+	time.AfterFunc(1*time.Second, cancel)
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -34,8 +63,8 @@ func main() {
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		atomic.AddInt64(&inputSum, i)   // Потокобезопасно увеличиваем сумму
+		atomic.AddInt64(&inputCount, 1) // Потокобезопасно увеличиваем количество
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -55,7 +84,16 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
+	for i := 0; i < NumOut; i++ {
+		wg.Add(1)
+		go func(in <-chan int64, i int) {
+			defer wg.Done()
+			for v := range in {
+				amounts[i]++ // Увеличиваем счётчик обработанных чисел
+				chOut <- v   // Передаём значение в chOut
+			}
+		}(outs[i], i)
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -68,23 +106,29 @@ func main() {
 	var sum int64   // сумма чисел результирующего канала
 
 	// 5. Читаем числа из результирующего канала
-	// ...
+	for v := range chOut {
+		count++  // Подсчитываем количество чисел
+		sum += v // Суммируем все числа
+	}
 
-	fmt.Println("Количество чисел", inputCount, count)
-	fmt.Println("Сумма чисел", inputSum, sum)
+	finalInputCount := atomic.LoadInt64(&inputCount)
+	finalInputSum := atomic.LoadInt64(&inputSum)
+
+	fmt.Println("Количество чисел", finalInputCount, count)
+	fmt.Println("Сумма чисел", finalInputSum, sum)
 	fmt.Println("Разбивка по каналам", amounts)
 
 	// проверка результатов
-	if inputSum != sum {
-		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum, sum)
+	if finalInputSum != sum {
+		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", finalInputSum, sum)
 	}
-	if inputCount != count {
-		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", inputCount, count)
+	if finalInputCount != count {
+		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", finalInputCount, count)
 	}
 	for _, v := range amounts {
-		inputCount -= v
+		finalInputCount -= v
 	}
-	if inputCount != 0 {
+	if finalInputCount != 0 {
 		log.Fatalf("Ошибка: разделение чисел по каналам неверное\n")
 	}
 }
